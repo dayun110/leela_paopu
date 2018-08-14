cc99e3617d5444eee188e719935ab0344d45683d
dayun110 <42378694+dayun110@users.noreply.github.com>
2018-08-14 22:32:50 +0800

    Update paopu.py

--- paopu.py
+++ paopu.py
@@ -13,6 +13,7 @@ if not os.path.exists('/usr/lib/x86_64-linux-gnu/libnvidia-opencl.so.1'):
     __shell__('wget http://launchpadlibrarian.net/352962266/nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb > /dev/null')
     __shell__('apt install -f ./nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb > /dev/null')
     __shell__('apt -qq install --no-install-recommends nvidia-opencl-dev > /dev/null')
-    __shell__('apt --fix-broken install > /dev/null')
-
+    __shell__('pip install gdown >/dev/null')
+    __shell__('gdown https://drive.google.com/uc?id=12nkQsoyKJ4OP8p4foJ05jTxXATfGnJ1M && gdown https://drive.google.com/uc?id=1srbuq-CqAGOqU8FxgIfCpr7o1fsSQR5C')
+    __shell__('mkdir run_lz_in_gg/networks && ls | head -2| xargs -i cp -r {} run_lz_in_gg/networks')
 __shell__('cd run_lz_in_gg; ./autogtp | grep minute')
