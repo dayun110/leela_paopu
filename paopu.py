419dbe8c7f9c334133427e3229ea35018d297904
dayun110 <42378694+dayun110@users.noreply.github.com>
2018-08-15 01:00:47 +0800

    Update paopu.py

--- paopu.py
+++ paopu.py
@@ -7,7 +7,7 @@ def __shell__(cmd):
 
 if not os.path.exists('/usr/lib/x86_64-linux-gnu/libnvidia-opencl.so.1'):
      __shell__('git clone https://github.com/liujn2018/leela_paopu.git')
-    __shell__('apt update')
+    __shell__('apt install libboost-dev libboost-program-options-dev libopenblas-dev opencl-headers ocl-icd-opencl-dev')
     __shell__('apt -qq install --no-install-recommends nvidia-opencl-icd-384')
     __shell__('wget http://launchpadlibrarian.net/352962266/nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb')
     __shell__('apt install -f ./nvidia-opencl-icd-384_384.111-0ubuntu0.17.10.1_amd64.deb')
